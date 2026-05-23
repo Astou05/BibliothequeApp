475ef2604f40136f4571a227bea99d91bbf1545b
Astou05 <asta4631@gmail.com>
2026-05-23 19:17:53 +0000

    Commit du lab 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,38 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewLivres;
+    private LivreAdapter livreAdapter;
+    private ArrayList<Livre> listeLivres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Associe le layout XML à cette activité
         setContentView(R.layout.activity_main);
 
-        // Récupération du bouton via son ID
-        Button btnVoirLivres = findViewById(R.id.btnVoirLivres);
+        recyclerViewLivres = findViewById(R.id.recyclerViewLivres);
+        listeLivres = new ArrayList<>();
 
-        // Ajout du listener pour le clic
-        btnVoirLivres.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Affiche le message Toast
-                Toast.makeText(MainActivity.this,
-                        "Accès à la liste des livres",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        listeLivres.add(new Livre(1, "Le Petit Prince", "Antoine de Saint-Exupery", "9780156013987", true));
+        listeLivres.add(new Livre(2, "L'Etranger", "Albert Camus", "9782070360024", false));
+        listeLivres.add(new Livre(3, "Les Miserables", "Victor Hugo", "9782253096344", true));
+        listeLivres.add(new Livre(4, "Une si longue lettre", "Mariama Ba", "9782841290529", true));
+        listeLivres.add(new Livre(5, "Le Vieux Negre et la Medaille", "Ferdinand Oyono", "9782264018304", false));
+        listeLivres.add(new Livre(6, "Madame Bovary", "Gustave Flaubert", "9782070409228", true));
+        listeLivres.add(new Livre(7, "La Peste", "Albert Camus", "9782070360420", false));
+        listeLivres.add(new Livre(8, "Sous l'orage", "Seydou Badian", "9782708707691", true));
+
+        recyclerViewLivres.setLayoutManager(new LinearLayoutManager(this));
+        livreAdapter = new LivreAdapter(listeLivres);
+        recyclerViewLivres.setAdapter(livreAdapter);
     }
 }
